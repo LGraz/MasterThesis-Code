15466e179dabfbdbc94addf89123ccf46a39f888
LukasGraz <lukasgraz99@gmail.com>
2022-06-03 13:09:41 +0200

    .

--- shell_scripts/send_to_sfs.sh
+++ shell_scripts/send_to_sfs.sh
@@ -3,13 +3,13 @@ M=/home/lukas/Documents/ETH/MASTER_THESIS
 
 rm -rf $M/temp
 mkdir -p $M/temp/code
-cp -r $M/code/{data,shell_scripts} $M/temp/
+cp -r $M/code/{data,shell_scripts} $M/temp/code/
 
 # convert to pickle and remove csv files for size
-python $M/temp/code/data/data_manipulation/yielmapping_to_pickle.py
+cd code; python $M/temp/code/data/data_manipulation/yielmapping_to_pickle.py; cd ..
 find $M/temp/code/data/yieldmapping_data -name  "*.csv" -type f -delete
 
 #copy files to remote
-scp -rC $M/temp/code lgraz@sftpmath.math.ethz.ch:/userdata/lgraz/thesis/
+scp -rC $M/temp/code lgraz@sftpmath.math.ethz.ch:/home/thesis/
 
 rm -rf $M/temp/code
