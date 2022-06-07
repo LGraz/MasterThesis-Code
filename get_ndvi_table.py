a5a51c3a711d2b081e48dc6176dd714f075ff156
LukasGraz <lukasgraz99@gmail.com>
2022-06-07 18:24:34 +0200

    .

--- get_ndvi_table.py
+++ get_ndvi_table.py
@@ -1,87 +1,11 @@
 # %%
 import os
 import sys
-import pandas as pd
-import pickle
 
 while "interpol" in os.getcwd():
     os.chdir("..")
 sys.path.append(os.getcwd())
-import my_utils.data_handle as data_handle
-import my_utils.itpl as itpl
-import my_utils.strategies as strategies
-from my_utils.pixel_multiprocess import pixel_multiprocess
-from my_utils.pixel_misc import get_pixel_info_df
 
-# settings
-update = False  # True = recalculate everything
-save = False
-x_axis = "gdd"
-frac = 0.0002
+from my_utils.get_ndvi_table import get_ndvi_table
 
-# load Data
-pixels_path = "data/computation_results/pixels_for_ndvi_table__" + \
-    x_axis + str(frac).replace(".", "") + ".pkl"
-if os.path.exists(pixels_path):
-    with open(pixels_path, "rb") as f:
-        pixels = pickle.load(f)
-else:
-    pixels = data_handle.get_pixels(
-        frac, cloudy=True, WW_cereals="cereals", seed=4321)
-    for pix in pixels:
-        pix.x_axis = x_axis
-
-
-# list with all interpolation options:
-# (tupel: itpl_args, dict: itpl_kwargs)
-itpl_methods_dict = [
-    (
-        ("ndvi_itpl_ss", itpl.smoothing_spline),
-        {"smooth": x_axis, "update": update,
-            "itpl_strategy": strategies.identity_no_extrapol},
-    ),
-    (
-        ("ndvi_itpl_loess", itpl.loess),
-        {"alpha": x_axis, "update": update,
-            "itpl_strategy": strategies.identity_no_extrapol},
-    ),
-    (
-        ("ndvi_itpl_dl", itpl.double_logistic),
-        {"itpl_strategy": strategies.identity,
-            "update": update, "opt_param": x_axis}
-    )
-]
-
-
-# %%
-
-
-def help_fun(pix):
-    df = get_pixel_info_df(pix, itpl_methods_dict)
-    return [df]
-
-
-df_list = pixel_multiprocess(pixels, help_fun)
-ndvi_table = pd.concat(df_list, axis=0, ignore_index=True)
-
-# %%
-# save computation results
-if save:
-    with open(pixels_path, "wb") as f:
-        pickle.dump(pixels, f)
-
-
-# ndvi_table_path = f"data/computation_results/ndvi_itpl_VS_observed_and_rest{frac}.pkl"
-# if not os.path.exists(ndvi_table_path):
-#     print("generate: " + ndvi_table_path)
-#     df_list = pixel_multiprocess(pixels, help_fun)
-#     result = pd.concat(df_list, axis=0, ignore_index=True)
-#     with open(ndvi_table_path, "wb") as f:
-#         pickle.dump(result, f)
-# else:
-#     print(ndvi_table_path + " already generated")
-
-
-# %%
-help_fun(pixels[0])
-# %%
+get_ndvi_table(1, update=True)
---
+++ my_utils/get_ndvi_table.py
@@ -0,0 +1,139 @@
+import pickle
+import pandas as pd
+import numpy as np
+import copy
+import os
+
+import my_utils.data_handle as data_handle
+import my_utils.itpl as itpl
+import my_utils.strategies as strategies
+import my_utils.strategies as strategies
+from my_utils.pixel_multiprocess import pixel_multiprocess
+
+
+def help_fun(pix, itpl_methods_dict):
+    "help-fun for multiprocessing"
+    df_pix_tpl = get_pixel_info_df(pix, itpl_methods_dict)
+    return [df_pix_tpl]
+
+
+def get_ndvi_table(frac, x_axis="gdd", update=False, save=True, return_pixels=False):
+    # first: itpl-methods
+    itpl_methods_dict = [
+        (
+            ("ndvi_itpl_ss_noex", itpl.smoothing_spline),
+            {"smooth": x_axis, "update": update,
+                "itpl_strategy": strategies.identity_no_extrapol},
+        ),
+        (
+            ("ndvi_itpl_loess_noex", itpl.loess),
+            {"alpha": x_axis, "update": update,
+                "itpl_strategy": strategies.identity_no_extrapol},
+        ),
+        (
+            ("ndvi_itpl_dl", itpl.double_logistic),
+            {"itpl_strategy": strategies.identity,
+                "update": update, "opt_param": x_axis}
+        )
+    ]
+    # now actual function
+    """
+    Here we calculate a big table of ndvi (out of bag) estimates using 
+    scl=[4,5], and various covariates
+    """
+
+    # load Data
+    pixels_path = "data/computation_results/pixels_for_ndvi_table__" + \
+        x_axis + str(frac).replace(".", "") + ".pkl"
+    if os.path.exists(pixels_path) and (not update):
+        with open(pixels_path, "rb") as f:
+            pixels = pickle.load(f)
+            print(f"{len(pixels)} modified Pixels have been loaded -----")
+    else:
+        pixels = data_handle.get_pixels(
+            frac, cloudy=True, WW_cereals="cereals", seed=4321)
+        for pix in pixels:
+            pix.x_axis = x_axis
+
+    # apply `get_pixel_info_df` to each pixel and save results
+    df_pix_tpl_list = pixel_multiprocess(pixels, help_fun, itpl_methods_dict)
+    ndvi_table = pd.concat([df_pix_tpl_list[i][0] for i in range(len(df_pix_tpl_list))],
+                           axis=0, ignore_index=True)
+    pixels = [df_pix_tpl_list[i][1] for i in range(len(df_pix_tpl_list))]
+
+    # save computation results
+    if save:
+        with open(pixels_path, "wb") as f:
+            pickle.dump(pixels, f)
+
+    # return
+    if return_pixels:
+        return (ndvi_table, pixels)
+    else:
+        return ndvi_table
+
+
+def get_pixel_info_df(pix, itpl_methods_dict):
+    """
+    this function produces a data frame for a pixel with the following info:
+    1. pixel.cov
+    2. ndvi observed & interpolated
+
+    returns: (DataFrame, pixel)  -- pixel is saved later to compute things only once
+    """
+    # add ndvi-observed
+    ndvi_observed = pd.DataFrame({"ndvi_observed": pix.get_ndvi()})
+
+    is_scl45 = (4 == pix.cov.scl_class.to_numpy()) | (
+        5 == pix.cov.scl_class.to_numpy())
+    ind_scl45 = np.where(is_scl45)
+    pix._prepare_itpl("_")
+
+    # get ndvi-itpl
+    drop_itpl_oob_labels = []
+    for itpl_args, itpl_kwargs in copy.deepcopy(itpl_methods_dict):
+        # fit with only using scl45
+        pix.itpl(*itpl_args, **itpl_kwargs)
+
+        # out-of-bag estimates for scl45
+        name = itpl_args[0]
+        itpl_args = itpl_args[1:]
+        itpl_kwargs_copy = itpl_kwargs.copy()
+        itpl_strategy = itpl_kwargs_copy.pop("itpl_strategy")
+        drop_itpl_oob_labels.append(name + "_out_of_bag")
+        out_of_bag = pix.itpl(
+            name + "_out_of_bag",
+            *itpl_args,
+            itpl_strategy=strategies.cv,
+            cv_strategy=itpl_strategy,
+            return_residuals=False,
+            **itpl_kwargs_copy,
+        )
+        out_of_bag = out_of_bag[pix.itpl_df.is_observation.to_numpy()[0]]
+        itpl_df = pix.itpl_df[pix.itpl_df.is_observation].reset_index(
+            drop=True)
+        temp = itpl_df[name].to_numpy()
+        temp[ind_scl45] = out_of_bag[
+            ~np.isnan(out_of_bag)
+        ]  # substitute with out_of_bag estim.
+        itpl_df[name] = temp
+
+    # concatenate dataframes with same number of rows
+    big_df = pd.concat(
+        [itpl_df, ndvi_observed, pix.cov.reset_index(drop=True)], axis=1)
+
+    # remove collumns
+    drop_labels = [
+        "is_observation",
+        "coord_id",
+        "FID",
+        "scene_id",
+        "product_uri",
+        "x_coord",
+        "y_coord",
+        "epsg",
+    ]
+    drop_labels.extend(drop_itpl_oob_labels)
+    for label in drop_labels:
+        big_df.drop(labels=label, axis=1, inplace=True)
+    return (big_df, pix)
