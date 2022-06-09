891b682dcc25e0fabe73a7b691d65f981fd292e7
LukasGraz <lukasgraz99@gmail.com>
2022-06-09 16:26:22 +0200

    .

--- ndvi_corr/get_ndvi_table.py
+++ ndvi_corr/get_ndvi_table.py
@@ -10,8 +10,8 @@ sys.path.append(os.getcwd())
 
 import my_utils.get_ndvi_table as get_ndvi_table
 
-get_ndvi_table.get_pixels_for_ndvi_table(0.01, update=False)
-# get_ndvi_table.get_pixels_for_ndvi_table(0.1, update=False)
+get_ndvi_table.get_pixels_for_ndvi_table(0.01, update=True)
+get_ndvi_table.get_pixels_for_ndvi_table(0.1, update=True)
 
 # %%
 
